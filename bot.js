039d2c5e2fc6d3f4dc2dff7021b0a57f9f5845fe
dramastore <georgehmariki@gmail.com>
2021-12-16 04:58:25 +0300

    sfd

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ bot.start(ctx => {
                                                         inline_keyboard: [
                                                             [
                                                                 { text: '🎖 My points', callback_data: 'points' },
-                                                                { text: '➕ Add points 🚀', url: `www.ohmyw.xyz/boost/${ctx.chat.id}/add` }
+                                                                { text: '➕ Add points', url: `www.ohmyw.xyz/boost/${ctx.chat.id}/add` }
                                                             ],
                                                             [
                                                                 { text: '🔙 Return to 💋 OH MY', url: 'https://t.me/joinchat/V6CN2nFJKa1JezKS' }
@@ -140,7 +140,7 @@ ${ctx.chat.first_name}
 
 Your remaing points is: ${user.points} pts.
 
-Click  "➕ Add points 🚀" button to increase your points
+Click  "➕ Add points" button to increase your points
 `
             ctx.answerCbQuery(text, {
                 show_alert: true
