f22c07f2ec7650ba9298e829d9b4d71612c8a603
gshemdoe <georgehmariki@gmail.com>
2022-09-24 04:31:17 +0300

    blogs added

--- bot.js
+++ bot.js
@@ -259,7 +259,9 @@ bot.on('channel_post', async ctx => {
                 '62ca86111afa2af6f7a1026c',
                 '62cd8fbe9de0786aafdb98b7',
                 '62df23671eef6dabf5feecde',
-                '63212e1f6eeba4e82a45bd27'
+                '63212e1f6eeba4e82a45bd27',
+                '632e5c7d2744c9849dd69c0a',
+                '632e58662744c9849dd69ba1'
             ]
             let rrnp = Math.floor(Math.random() * posts.length)
             let op2link = `https://font5.net/blog/post.html?id=${posts[rrnp]}#getting-full-show-showid=${cdata}`
